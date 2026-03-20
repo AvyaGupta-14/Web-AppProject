f61c5a632ef4fae7a27127dc4f991260d3e0a958
AvyaGupta-14 <avyagupta7@gmail.com>
2026-03-20 15:19:31 +0530

    update

--- src/main/java/com/spring/mvc/controller/UserController.java
+++ src/main/java/com/spring/mvc/controller/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.spring.mvc.model.Employee;
+
 //import com.spring.mvc.model.Employee;
 
 @Controller
@@ -42,11 +44,11 @@ public class UserController {
 	
 	
 	
-//	@PostMapping("/createUser")
-//	public String createUser(@ModelAttribute Employee employee)
-//	{
-//		System.out.println(employee);
-//		return "index";
-//	}
+	@PostMapping("/createUser")
+	public String createUser(@ModelAttribute Employee employee)
+	{
+		System.out.println(employee);
+		return "index";
+	}
 
 }
